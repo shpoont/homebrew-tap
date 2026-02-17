df59788de42e5d1057194851916fdf329219bd8e
Leon Komarovsky <leonid@komarovsky.info>
2026-02-17 02:32:39 +0000

    Remove tap README and modernize ism formula

--- Formula/ism.rb
+++ Formula/ism.rb
@@ -1,30 +1,27 @@
 class Ism < Formula
   desc "Interactive Shell Monitor"
   homepage "https://github.com/shpoont/ism"
-  url "https://github.com/shpoont/ism/archive/v0.1.tar.gz"
+  url "https://github.com/shpoont/ism/archive/refs/tags/v0.1.tar.gz"
   sha256 "bd929defb5e99b1d1e004aa271d3980ac7044786641b69d80f2505c6b24dc389"
 
   head "https://github.com/shpoont/ism.git"
 
-  bottle :unneeded
-
-  depends_on "jq"
   depends_on "bash-preexec"
+  depends_on "jq"
 
   def install
     (prefix/"etc/profile.d").install "ism.sh"
   end
 
-  def caveats; <<-EOS.undent
-    Add the following line to your bash profile (e.g. ~/.bashrc, ~/.profile, or ~/.bash_profile)
+  def caveats
+    <<~EOS
+      Add the following line to your bash profile (e.g. ~/.bashrc, ~/.profile, or ~/.bash_profile):
 
-      [[ -f $(brew --prefix)/etc/profile.d/ism.sh ]] && . $(brew --prefix)/etc/profile.d/ism.sh
-
+        [[ -f #{opt_prefix}/etc/profile.d/ism.sh ]] && . #{opt_prefix}/etc/profile.d/ism.sh
     EOS
   end
 
   test do
-    # Just testing that the file is installed
-    assert File.exist?("#{prefix}/etc/profile.d/ism.sh")
+    assert_path_exists prefix/"etc/profile.d/ism.sh"
   end
 end
