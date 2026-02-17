a6ef011177e1b9abaee5ceec34230fb39c5f4c28
Leon Komarovsky <leonid@komarovsky.info>
2026-02-17 02:50:45 +0000

    Add arm64_tahoe bottle for dotfiles-manager 0.1.1

--- Formula/dotfiles-manager.rb
+++ Formula/dotfiles-manager.rb
@@ -4,6 +4,12 @@ class DotfilesManager < Formula
   url "https://github.com/shpoont/dotfiles-manager/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "0fe09950f2a6c498ece53374b8b09561ea2d4e93e72c524c7e2aa65e5863f79a"
 
+  bottle do
+    root_url "https://github.com/shpoont/homebrew-tap/releases/download/dotfiles-manager-0.1.1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "fb7c02ec63af549324a81048de64e7bcc92f0ed400e0634f74dd3ae59d2b3d89"
+  end
+
   depends_on "go" => :build
 
   def install
